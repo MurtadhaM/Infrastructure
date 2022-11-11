f1c0c09e753ceba0448e3c6fffb4d5b2f6fc5611
Murtadha Marzouq <mmarzou0@gmail.com>
2022-11-11 00:21:49 -0500

    👀

--- Automation/Terraform/Example/main.tf
+++ Automation/Terraform/Example/main.tf
@@ -1,10 +1,10 @@
 resource "local_file" "NewFile"{
 	filename = var.filename
-	content = var.content
+	content = "The resource is ${random_pet.my-resource.id}"
 
 }
 
-resource "random_pet" "my-pet"{
+resource "random_pet" "my-resource"{
 
 prefix = var.prefix
 separator = var.separator
